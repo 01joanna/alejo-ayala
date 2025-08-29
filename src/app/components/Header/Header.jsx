c194d2510a32aa1afbf435198967f21d78d5c359
01joanna <joa.cuevas9@gmail.com>
2025-08-29 11:26:52 +0200

    updated header on work pages

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -9,27 +9,27 @@ export default function Header({ scrollY, onAboutClick, showAbout }) {
     const pathname = usePathname();
 
     const isHome = pathname === "/";
+    const isWorkPage = pathname.startsWith("/work/");
 
     useEffect(() => {
         if (showAbout) {
             controls.start({ top: "20%", y: 0, transition: { duration: 0.5 } });
-        } else if (!isHome) {
+        } else if (isWorkPage) {
             controls.start({ top: "0px", y: 0, transition: { duration: 0.5 } });
-        } else {
+        } else if (isHome) {
             controls.start({
                 top: scrollY > 10 ? "0px" : "50%",
                 y: scrollY > 10 ? 0 : "-50%",
                 transition: { duration: 0.5 }
             });
         }
-    }, [scrollY, showAbout, isHome, controls]);
-
+    }, [scrollY, showAbout, isHome, isWorkPage, controls]);
 
     const handleHomeClick = () => {
         if (!isHome) {
             router.push("/");
         } else {
-            window.scrollTo({ top: 0, behavior: "smooth" }); 
+            window.scrollTo({ top: 0, behavior: "smooth" });
         }
     };
 
@@ -44,17 +44,21 @@ export default function Header({ scrollY, onAboutClick, showAbout }) {
     return (
         <motion.header
             animate={controls}
-            initial={{ top: "50%", y: "-50%" }}
-            className="fixed left-0 w-full px-8 py-6 bg-transparent uppercase text-md tracking-widest text-white z-50 font-helveticaLight"
+            initial={{ 
+                top: isHome ? "50%" : "0px", 
+                y: isHome ? "-50%" : "0" 
+            }}
+            className={`${isWorkPage ? "absolute mt-15" : "fixed"} left-0 w-full px-8 py-6 bg-transparent uppercase text-md tracking-widest text-white z-50 font-helveticaLight`}
         >
             <div className="max-w-7xl w-full mx-auto flex items-center justify-evenly">
-                
-            <button className="cursor-pointer" onClick={handleWorkClick}>WORK</button>
-            <h1 className="font-helveticaBold cursor-pointer tracking-widest"
-            onClick={handleHomeClick}>Alejo Ayala</h1>
-            <button className="cursor-pointer" onClick={onAboutClick}>ABOUT</button>
-
-
+                <button className="cursor-pointer" onClick={handleWorkClick}>WORK</button>
+                <h1 
+                    className="font-helveticaBold cursor-pointer tracking-widest"
+                    onClick={handleHomeClick}
+                >
+                    Alejo Ayala
+                </h1>
+                <button className="cursor-pointer" onClick={onAboutClick}>ABOUT</button>
             </div>
         </motion.header>
     );
